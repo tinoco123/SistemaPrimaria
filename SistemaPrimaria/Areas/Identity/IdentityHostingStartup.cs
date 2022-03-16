165a0ceb076c05171a15818f13f3f49db2bc3c0c
tinoco123 <tinocojona@outlook.com>
2022-03-16 01:07:40 -0600

    Sistema de Login y Register

---
+++ SistemaPrimaria/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaPrimaria.Areas.Identity.Data;
+using SistemaPrimaria.Data;
+
+[assembly: HostingStartup(typeof(SistemaPrimaria.Areas.Identity.IdentityHostingStartup))]
+namespace SistemaPrimaria.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SistemaPrimariaDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SistemaPrimariaDBContextConnection")));
+
+                services.AddDefaultIdentity<SistemaPrimariaAdministrator>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<SistemaPrimariaDBContext>();
+            });
+        }
+    }
+}
--- SistemaPrimaria/Startup.cs
+++ SistemaPrimaria/Startup.cs
@@ -24,6 +24,7 @@ namespace SistemaPrimaria
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +44,7 @@ namespace SistemaPrimaria
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +52,7 @@ namespace SistemaPrimaria
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
