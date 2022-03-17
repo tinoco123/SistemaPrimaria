c437b261ed81413717061e4ff081686dfc93e656
tinoco123 <tinocojona@outlook.com>
2022-03-17 11:11:47 -0600

    Base de datos, autorizacion para entrar

--- SistemaPrimaria/Controllers/EstudiantesController.cs
+++ SistemaPrimaria/Controllers/EstudiantesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using SistemaPrimaria.Models;
 
 namespace SistemaPrimaria.Controllers
 {
+    [Authorize]
     public class EstudiantesController : Controller
     {
         private readonly SistemaPrimariaDB _context;
@@ -18,7 +20,7 @@ namespace SistemaPrimaria.Controllers
         {
             _context = context;
         }
-
+        
         // GET: Estudiantes
         public async Task<IActionResult> Index()
         {
---
+++ SistemaPrimaria/Migrations/SistemaPrimariaDBMigrations/20220317170749_initial-database.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SistemaPrimaria.Migrations.SistemaPrimariaDBMigrations
+{
+    public partial class initialdatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estudiante",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdGrupo = table.Column<int>(nullable: false),
+                    Nombre = table.Column<string>(nullable: true),
+                    ApellidoPaterno = table.Column<string>(nullable: true),
+                    ApellidoMaterno = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiante", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Maestro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(nullable: true),
+                    ApellidoPaterno = table.Column<string>(nullable: true),
+                    ApellidoMaterno = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maestro", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Materia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materia", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estudiante");
+
+            migrationBuilder.DropTable(
+                name: "Maestro");
+
+            migrationBuilder.DropTable(
+                name: "Materia");
+        }
+    }
+}
