32b9b43f82169b19005e773868bf12dc6a23ce94
tinoco123 <tinocojona@outlook.com>
2022-03-17 11:01:50 -0600

    Model Maestro

---
+++ SistemaPrimaria/Models/Maestro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaPrimaria.Models
+{
+    public class Maestro
+    {
+        public Maestro()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
