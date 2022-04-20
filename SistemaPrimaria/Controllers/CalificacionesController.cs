a58bb5c32a6f74d8f2b0fd39167fe38b32739b66
tinoco123 <tinocojona@outlook.com>
2022-04-19 19:22:51 -0500

    Calificaciones Create

--- SistemaPrimaria/Controllers/CalificacionesController.cs
+++ SistemaPrimaria/Controllers/CalificacionesController.cs
@@ -20,28 +20,12 @@ namespace SistemaPrimaria.Controllers
         }
 
         // GET: Calificacions
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Calificacion.ToListAsync());
-        }
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _context.Calificacion.ToListAsync());
+        //}
 
-        // GET: Calificacions/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var calificacion = await _context.Calificacion
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (calificacion == null)
-            {
-                return NotFound();
-            }
-
-            return View(calificacion);
-        }
+        
 
         // GET: Calificacions/Create
         public IActionResult Create()
@@ -65,85 +49,7 @@ namespace SistemaPrimaria.Controllers
             return View(calificacion);
         }
 
-        // GET: Calificacions/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var calificacion = await _context.Calificacion.FindAsync(id);
-            if (calificacion == null)
-            {
-                return NotFound();
-            }
-            return View(calificacion);
-        }
-
-        // POST: Calificacions/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,calificacion,IdEstudiante,IdMateria")] Calificacion calificacion)
-        {
-            if (id != calificacion.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(calificacion);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CalificacionExists(calificacion.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(calificacion);
-        }
-
-        // GET: Calificacions/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var calificacion = await _context.Calificacion
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (calificacion == null)
-            {
-                return NotFound();
-            }
-
-            return View(calificacion);
-        }
-
-        // POST: Calificacions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var calificacion = await _context.Calificacion.FindAsync(id);
-            _context.Calificacion.Remove(calificacion);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        
 
         private bool CalificacionExists(int id)
         {
